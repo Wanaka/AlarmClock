8f3c886d4fedeae79bb735c2a9d1e3497f22650f
Wanaka <jonas.haag@hotmail.com>
2020-09-01 17:34:37 +0200

    cleaned up code

---
+++ app/src/androidTest/java/com/example/alarmclock/view/alarmClock/AlarmClockFragmentTest.kt
@@ -0,0 +1,17 @@
+package com.example.alarmclock.view.alarmClock
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class AlarmClockFragmentTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
